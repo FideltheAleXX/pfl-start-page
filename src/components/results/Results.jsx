80c33f3c9c4b271be1bf8db4f84b3dacd5017028
theAleXX <th3alexx@gmail.com>
2026-04-10 18:23:44 +0200

    start to develop Results component

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,6 +1,23 @@
 import styles from './Results.module.css';
+import { AiOutlineVideoCamera } from 'react-icons/ai';
+
 const Results = () => {
-  return <h1>Results</h1>;
+  return (
+    <section className={styles.results}>
+      <div className={styles.tableHeader}>Тур 23</div>
+      <div className={styles.table} role="table">
+        <div role="row" className={styles.headerRow}>
+          <div>Дата</div>
+          <div>Господарі</div>
+          <div>Результат</div>
+          <div>Гості</div>
+          <div>
+            <AiOutlineVideoCamera color="#0db04b" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Results;
