e47cc529f502936d1ae104fa1ca333c1d544b543
theAleXX <th3alexx@gmail.com>
2026-04-16 18:33:06 +0200

    add Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -16,62 +16,76 @@ const Footer = () => {
       <div className={styles.socials}>
         <div className={styles.social}>
           <a
-            className={styles.socialLink}
             href="https://www.facebook.com/pfl.ua"
             rel="noopener noreferrer"
             target="_blank"
           >
-            <FaFacebook color="green" />
+            <FaFacebook color="#0db04b" />
           </a>
         </div>
         <div className={styles.social}>
           <a
-            className={styles.socialLink}
             href="https://www.instagram.com/pfl.ua/"
             rel="noopener noreferrer"
             target="_blank"
           >
-            <FaInstagram />
+            <FaInstagram color="#0db04b" />
           </a>
         </div>
         <div className={styles.social}>
           <a
-            className={styles.socialLink}
             href="https://www.youtube.com/user/PFLUkraine"
             rel="noopener noreferrer"
             target="_blank"
           >
-            <FaYoutube />
+            <FaYoutube color="#0db04b" />
           </a>
         </div>
         <div className={styles.social}>
           <a
-            className={styles.socialLink}
             href="https://x.com/PFL_Ukraine"
             rel="noopener noreferrer"
             target="_blank"
           >
-            <FaTwitter />
+            <FaTwitter color="#0db04b" />
           </a>
         </div>
         <div className={styles.social}>
           <a
-            className={styles.socialLink}
             href="https://t.me/pfl_ukraine"
             rel="noopener noreferrer"
             target="_blank"
           >
-            <FaTelegram />
+            <FaTelegram color="#0db04b" />
           </a>
         </div>
       </div>
-      <div className={styles.docs}></div>
+      <div className={styles.docs}>
+        <a className={styles.docLink} href="https://pfl.ua/document">
+          Документи
+        </a>
+        <a className={styles.docLink} href="https://pfl.ua/partners">
+          Партнери
+        </a>
+        <a className={styles.docLink} href="https://pfl.ua/custom/5">
+          Контакти
+        </a>
+      </div>
       <div className={styles.info}>
-        Назва онлайн-медіа: офіційний інтернет сайт «ПРОФЕСІОНАЛЬНА ФУТБОЛЬНА
-        ЛІГА» Ідентифікатор онлайн-медіа у Реєстрі: R40-06347 Адреса реєстрації
-        та інші засоби зв'язку з онлайн-медіа: ТОВ «ЛІГА ФУТБОЛ», пров.
-        Лабораторний, буд. 7-а, м. Київ, 01133; (044) 356 82 05; lf@pfl.ua
-        21+{' '}
+        <p>
+          Назва онлайн-медіа: офіційний інтернет сайт «ПРОФЕСІОНАЛЬНА ФУТБОЛЬНА
+          ЛІГА»
+        </p>
+        <p> Ідентифікатор онлайн-медіа у Реєстрі: R40-06347</p>
+        <p>
+          Адреса реєстрації та інші засоби зв'язку з онлайн-медіа: ТОВ «ЛІГА
+          ФУТБОЛ»,
+        </p>
+        <p>
+          пров. Лабораторний, буд. 7-а, м. Київ, 01133; (044) 356 82 05;
+          lf@pfl.ua
+        </p>
+        <p>21+</p>
       </div>
     </footer>
   );
